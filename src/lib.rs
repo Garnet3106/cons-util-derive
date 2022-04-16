8fa5ce6f539d5266c853a4cc4797db59ad3c2e58
Garnet3106 <manage@gant.work>
2022-04-16 17:37:13 +0900

    fix: pattern match in translate()

--- src/lib.rs
+++ src/lib.rs
@@ -14,23 +14,23 @@ pub fn derive_log(input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as DeriveInput);
     let option = LogTranslationOption::from_derive_input(&item).expect("Failed to parse derive input.");
     let enum_name = item.ident;
-    let mut fns = quote!{};
+    let mut variant_ptts = quote!{};
 
     match option.data {
         darling::ast::Data::Enum(variant_options) => {
             for each_variant_option in variant_options {
+                let variant_name = each_variant_option.ident;
+                let mut lang_ptts = quote!{};
+
                 macro_rules! append_impl {
                     ($lang_name:expr, $field_name:ident) => {
                         match each_variant_option.$field_name {
                             Some(translation_result) => {
-                                let fn_ident = Ident::new(&format!("translate_{}_to_{}", option.ident, $lang_name), Span::call_site());
-                                let new_gen = quote!{
-                                    pub fn #fn_ident() -> String {
-                                        return #translation_result.to_string();
-                                    }
+                                let new_lang_ptt = quote!{
+                                    $lang_name => #translation_result,
                                 };
 
-                                fns.append_all(vec![new_gen]);
+                                lang_ptts.append_all(vec![new_lang_ptt]);
                             },
                             None => (),
                         }
@@ -39,12 +39,35 @@ pub fn derive_log(input: TokenStream) -> TokenStream {
 
                 append_impl!("en", en);
                 append_impl!("ja", ja);
+
+                let new_variant_ptt = quote!{
+                    #enum_name::#variant_name => {
+                        match lang {
+                            #lang_ptts
+                            _ => "<UNKNOWN_LANGUAGE>",
+                        }
+                    },
+                };
+
+                variant_ptts.append_all(vec![new_variant_ptt]);
             }
         },
         _ => panic!("Expected an enum."),
     };
 
-    let gen = quote!{impl #enum_name {#fns}};
+    let fn_ident = Ident::new("translate", Span::call_site());
+
+    let gen = quote!{
+        impl #enum_name {
+            pub fn #fn_ident(&self, lang: &str) -> &str {
+                return match self {
+                    #variant_ptts
+                    _ => "<UNKNOWN_LOG>",
+                };
+            }
+        }
+    };
+
     return gen.into();
 }
 
@@ -58,6 +81,7 @@ struct LogTranslationOption {
 #[derive(Clone, Debug, FromVariant)]
 #[darling(attributes(translate))]
 struct LogTranslationVariantOption {
+    ident: Ident,
     #[darling(default)]
     en: Option<String>,
     #[darling(default)]
